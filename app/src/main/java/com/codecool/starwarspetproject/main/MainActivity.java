bb0d9ff0c431e52f844402ba56a9f88d40fd8917
Drosztmér István <drosztmer.istvan@gmail.com>
2020-10-27 10:56:50 +0100

    Remove dagger, add butterknife dependency

--- app/src/main/java/com/codecool/starwarspetproject/main/MainActivity.java
+++ app/src/main/java/com/codecool/starwarspetproject/main/MainActivity.java
@@ -5,12 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.codecool.starwarspetproject.R;
+import com.codecool.starwarspetproject.model.Character;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements MainContract.View {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void setValues(List<Character> characters) {
+
+    }
+
+    @Override
+    public void showProgress() {
+
+    }
+
+    @Override
+    public void hideProgress() {
+
+    }
+
+    @Override
+    public void onError() {
+
     }
 }
